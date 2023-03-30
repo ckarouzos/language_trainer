3ed96390c9afe50563391d40533e54cc5fccf326
Constantinos Karouzos <ckarouzos@gmail.com>
2023-03-30 19:16:50 +0100

    MLM training

---
+++ modules/mlm_pretraining.py
@@ -0,0 +1,79 @@
+from typing import Optional
+
+import torch
+from pytorch_lightning import LightningModule
+from torch.nn import ModuleDict
+from transformers import (
+    AdamW,
+    AutoConfig,
+    AutoModelForPreTraining,
+    get_linear_schedule_with_warmup,
+)
+
+class Transformer_MLM(LightningModule):
+
+    def __init__(
+        self,
+        model_name_or_path: str,
+        learning_rate: float = 6e-4,
+        adam_epsilon: float = 1e-8,
+        warmup_steps: int = 0,
+        weight_decay: float = 0.01,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        eval_splits: Optional[list] = None,
+        **kwargs,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.config = AutoConfig.from_pretrained(model_name_or_path)
+        self.model = AutoModelForPreTraining.from_pretrained(model_name_or_path, config=self.config)
+
+    def forward(self, input_ids, attention_mask):
+        return self.model(input_ids=input_ids, attention_mask=attention_mask, labels=input_ids)
+
+    def training_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        outputs = self(input_ids=input_ids, attention_mask=attention_mask)
+        loss = outputs[0]
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        outputs = self(input_ids=input_ids, attention_mask=attention_mask)
+        val_loss = outputs[0]
+        self.log("val_loss", val_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+        x = {"loss": val_loss}
+        return x
+
+    def validation_epoch_end(self, outputs):
+        loss = torch.stack([x["loss"] for x in outputs]).mean()
+        self.log("avg_val_loss", loss, prog_bar=True)
+
+    def configure_optimizers(self):
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            }
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
+
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.hparams.warmup_steps,
+            num_training_steps=self.trainer.estimated_stepping_batches,
+        )
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        return [optimizer], [scheduler]
--- trainers/trainer.py
+++ trainers/trainer.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 from datamodules.supergluedatamodule import SuperGLUEDataModule, SuperGLUEDataModule_record
 from modules.finetune import superGLUE_Transformer, superGLUE_Transformer_record
+from modules.mlm_pretraining import Transformer_MLM
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import TQDMProgressBar
@@ -10,12 +11,14 @@ from pytorch_lightning.callbacks import TQDMProgressBar
 if __name__ =="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--task_name", type=str, default="wic") 
-    parser.add_argument("--model_name_or_path", type=str, default="../models/roberta-base")
-    parser.add_argument("--max_epochs", type=int, default=10)
+    parser.add_argument("--model_name_or_path", type=str, default="roberta-base")
+    parser.add_argument("--max_epochs", type=int, default=20)
+    parser.add_argument("--mlm", default=False, action="store_true")
     args = parser.parse_args()
     task_name = args.task_name
     model_name_or_path = args.model_name_or_path
     max_epochs = args.max_epochs
+    mlm = args.mlm
     seed_everything(42)
     if task_name == "record":
         dm = SuperGLUEDataModule_record(model_name_or_path, task_name=task_name)
@@ -23,10 +26,13 @@ if __name__ =="__main__":
         dm = SuperGLUEDataModule(model_name_or_path, task_name=task_name)
     dm.prepare_data()
     dm.setup("fit")
-    if task_name == "record":
-        model = superGLUE_Transformer_record(model_name_or_path, num_labels=dm.num_labels, eval_splits=dm.eval_splits, task_name=dm.task_name,)
+    if mlm:
+        model = Transformer_MLM(model_name_or_path, eval_splits=dm.eval_splits,)
     else:
-        model = superGLUE_Transformer(model_name_or_path, num_labels=dm.num_labels, eval_splits=dm.eval_splits, task_name=dm.task_name,)
+        if task_name == "record":
+            model = superGLUE_Transformer_record(model_name_or_path, num_labels=dm.num_labels, eval_splits=dm.eval_splits, task_name=dm.task_name,)
+        else:
+            model = superGLUE_Transformer(model_name_or_path, num_labels=dm.num_labels, eval_splits=dm.eval_splits, task_name=dm.task_name,)
 
     tb_logger = pl_loggers.TensorBoardLogger(save_dir="./", log_graph=True)
     trainer = Trainer(
